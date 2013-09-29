c65cefd621d2e821af17a6a6fe07b73fb5ac937f
srinivasnangunuri <sxn127730@utdallas.edu>
2013-09-29 15:57:35 -0500

    version7

---
+++ PHP/Forms/Input.php
@@ -0,0 +1,12 @@
+
+<html>
+<head><title>My Form </title></head>
+<body>
+<br/>
+<form>
+First Name : <input fname="firstName"/><br/>
+Last Name: <input lname="lastName"/> <br/>
+<input type="submit" value="submit"/>
+</form
+</body>
+</html>
