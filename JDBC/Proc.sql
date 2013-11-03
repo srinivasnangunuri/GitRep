f911e83cdf48f122278c50542ef520ffb5d7c7d5
srinivasnangunuri <sxn127730@utdallas.edu>
2013-11-03 14:48:13 -0600

    V7

--- JDBC/Proc.sql
+++ JDBC/Proc.sql
@@ -2,11 +2,11 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS `firstdb`.`getEmpName` $$
 CREATE PROCEDURE `firstdb`.`getEmpName`
-   (IN Employee_ID INT, OUT EMP_FIRST VARCHAR(50))
+   (IN ID INT, OUT EMP_FIRST VARCHAR(50))
 BEGIN
    SELECT fname INTO EMP_FIRST
    FROM Employee_rec
-   WHERE ID = Employee_ID;
+   WHERE employee_ID = ID;
 END $$
 
 DELIMITER ;
